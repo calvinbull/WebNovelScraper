5ef55ff9c0c3f145473177d303ceab8ac7ecfd00
Calvin Bull <97459530+calvinbull@users.noreply.github.com>
2023-04-13 18:18:40 -0400

    Pool / Queue / Objects
    Altered base method of obtaining chapter lists based on index page. Titles and URLs are now stored as objects and added to a queue. Need to implement pool threads still.

--- scraper.py
+++ scraper.py
@@ -1,40 +1,88 @@
 #!/usr/bin/env python
-import requests
-from os import path, makedirs, getenv
-from bs4 import BeautifulSoup
 
-# URL = "https://free-webnovel.com/freewebnovel/super-gene/chapter-1"
-URL = input("Input URL to first desired chapter: ").strip()
+import requests
+from os import path, makedirs, getenv, cpu_count
+from bs4 import BeautifulSoup
+from multiprocessing import Pool, Queue
+
+
+URL = "https://free-webnovel.com/freewebnovel/super-gene/"
+# URL = input("Input exact URL to novel page (e.g. https://free-webnovel.com/freewebnovel/novel-name/): ").strip()
 
 # Dictionary of HTTP headers
 headers = {'User-Agent': 'helloworld'}
 
-# Initialize nextchapter for loop
-nextchapter = "temp"
+# Pull initial page
+response = requests.get(URL, headers=headers)
+# Raise exception if unsuccessful
+response.raise_for_status()
+
+# Turn text response into soup
+soup = BeautifulSoup(response.text, "html5lib")
+
+# Parse soup
+titlename = soup.find("meta", {"property": "og:novel:novel_name"}).get("content")
+short_title = titlename.lower().replace(' ','-')
+imglink = soup.find("meta", {"property": "og:image"}).get("content")
+
+all_chapters = f"https://free-webnovel.com/ajax/chapter-archive?novelId={short_title}"
+
+# Prepare folder
+folder = path.join(getenv('HOME'), 'WebNovelScraper', titlename)
+if not path.exists(folder):
+    makedirs(folder)
+
+# Pull all chapters page
+response = requests.get(all_chapters, headers=headers)
+# Raise exception if unsuccessful
+response.raise_for_status()
+
+# Create new soup for chapter list
+soup = BeautifulSoup(response.text, "html5lib")
 
 
-while(nextchapter != ""):
-    # Pull page
-    response = requests.get(URL, headers=headers)
-    # Raise exception if unsuccessful
+# Pull chapter names/links from soup
+class chapter():
+    def __init__(self, name, URL, num):
+        self.name = name
+        self.URL = URL
+        self.num = num
+
+
+# Fill queue with chapter objects
+q = Queue()
+global q
+
+counter = 1
+for a in soup.find_all("a", href=True):
+    chapt = chapter(a.span.contents[0], a['href'], counter)
+    counter += 1
+    q.put(chapt)
+    
+
+# Function to write chapter files using info from queue
+def chapter_writer(chapt):
+    # Pull chapter page; exception if unsuccessful
+    response = requests.get(chapt.URL, headers=headers)
     response.raise_for_status()
 
     # Turn text response into soup
-    soup = BeautifulSoup(response.text, 'html.parser')
+    soup = BeautifulSoup(response.text, 'html5lib')
 
-    # Parse chapter soup
-    nextchapter = soup.find("a", {"id": "next_chap"}).get("href")
-    titlename = soup.find("a", {"class": "novel-title"}).string.strip()
-    chaptername = soup.find("a", {"class": "chr-title"}).string.strip()
+    # Parse chapter soup, write to file
     chaptertext = soup.find("div", attrs={"id": "chr-content"}).get_text("\n\n",True)
-
-    # Create folder for chapters if needed
-    folder = path.join(getenv('HOME'), 'WebNovelScraper', titlename)
-    if not path.exists(folder):
-        makedirs(folder)
-
-    # Write new chapter
-    open(f"{folder}/{titlename} - {chaptername}.txt", "w").write(f"{titlename}\n{chaptername}\n{chaptertext}")
-    print(f"Wrote {chaptername}")
-    URL = nextchapter
+       
+    with open(f"{folder}/{titlename} - {chapt.name}.txt", "w") as file:
+        file.write(f"{titlename}\n{chapt.name}\n{chaptertext}")
     
+    print(f"{chapt.name} - Complete")
+
+
+# TODO: multiprocessing https://docs.python.org/3/library/multiprocessing.html
+
+if __name__ == '__main__':
+    # start worker processes
+    with Pool(processes=cpu_count()) as pool:
+        pool.apply_async(chapter_writer(),(q,))
+        # while not q.empty():
+            # chapter_writer(q.get())       
