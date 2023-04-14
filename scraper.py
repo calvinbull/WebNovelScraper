744aaffe09d2c5611ef6e735266dbcaac9c5a6fc
Calvin Bull <97459530+calvinbull@users.noreply.github.com>
2023-04-14 13:14:22 -0400

    Update scraper.py
    Implemented multiprocessing. No longer using chapter class due to errors with allowable object size when using pool mapping. Still need to implement epub formatting.

--- scraper.py
+++ scraper.py
@@ -4,85 +4,96 @@ import requests
 from os import path, makedirs, getenv, cpu_count
 from bs4 import BeautifulSoup
 from multiprocessing import Pool, Queue
+from time import time, sleep
 
 
-URL = "https://free-webnovel.com/freewebnovel/super-gene/"
-# URL = input("Input exact URL to novel page (e.g. https://free-webnovel.com/freewebnovel/novel-name/): ").strip()
-
-# Dictionary of HTTP headers
-headers = {'User-Agent': 'helloworld'}
-
-# Pull initial page
-response = requests.get(URL, headers=headers)
-# Raise exception if unsuccessful
-response.raise_for_status()
-
-# Turn text response into soup
-soup = BeautifulSoup(response.text, "html5lib")
-
-# Parse soup
-titlename = soup.find("meta", {"property": "og:novel:novel_name"}).get("content")
-short_title = titlename.lower().replace(' ','-')
-imglink = soup.find("meta", {"property": "og:image"}).get("content")
-
-all_chapters = f"https://free-webnovel.com/ajax/chapter-archive?novelId={short_title}"
-
-# Prepare folder
-folder = path.join(getenv('HOME'), 'WebNovelScraper', titlename)
-if not path.exists(folder):
-    makedirs(folder)
-
-# Pull all chapters page
-response = requests.get(all_chapters, headers=headers)
-# Raise exception if unsuccessful
-response.raise_for_status()
-
-# Create new soup for chapter list
-soup = BeautifulSoup(response.text, "html5lib")
-
-
-# Pull chapter names/links from soup
+# Chapt class
 class chapter():
-    def __init__(self, name, URL, num):
+    def __init__(self, name, URL):
         self.name = name
         self.URL = URL
-        self.num = num
-
-
-# Fill queue with chapter objects
-q = Queue()
-global q
-
-counter = 1
-for a in soup.find_all("a", href=True):
-    chapt = chapter(a.span.contents[0], a['href'], counter)
-    counter += 1
-    q.put(chapt)
-    
 
 # Function to write chapter files using info from queue
 def chapter_writer(chapt):
-    # Pull chapter page; exception if unsuccessful
-    response = requests.get(chapt.URL, headers=headers)
+    try:
+        print(f"Processing chapter {chapt[0]}")
+        
+        # Pull chapter page; exception if unsuccessful
+        retries = 0
+        while True:
+            try:
+                response = requests.get(chapt[1], headers=headers, timeout=3)
+                break
+            except Exception:
+                retries += 1
+                wait = retries * 2
+                print(f"Request for chapter {chapt[0]} unsucessful. Waiting {wait} secs and re-trying...")
+                sleep(wait)
+                continue
+
+        response.raise_for_status()
+        
+        # Turn text response into soup
+        soup = BeautifulSoup(response.text, 'html5lib')
+
+        # Parse chapter soup, write to file
+        chaptertext = soup.find("div", attrs={"id": "chr-content"}).get_text("\n\n",True)
+        
+        with open(f"{folder}/{titlename} - {chapt[0]}.txt", "w") as file:
+            file.write(f"{titlename}\n\n{chaptertext}")
+        
+        print(f"{chapt[0]} - Complete")
+
+    except Exception as e:
+        print(f"Error processing chapter {chapt[0]}: {str(e)}")
+
+
+if __name__ == '__main__':
+
+    URL = "https://free-webnovel.com/freewebnovel/super-gene/"
+    # URL = input("Input exact URL to novel page (e.g. https://free-webnovel.com/freewebnovel/novel-name/): ").strip()
+
+    # Dictionary of HTTP headers
+    headers = {'User-Agent': 'helloworld'}
+    # Pull initial page
+    response = requests.get(URL, headers=headers)
+    # Raise exception if unsuccessful
     response.raise_for_status()
 
     # Turn text response into soup
-    soup = BeautifulSoup(response.text, 'html5lib')
+    soup = BeautifulSoup(response.text, "html5lib")
+    # Parse soup
+    titlename = soup.find("meta", {"property": "og:novel:novel_name"}).get("content")
+    short_title = titlename.lower().replace(' ','-')
+    imglink = soup.find("meta", {"property": "og:image"}).get("content")
+    all_chapters = f"https://free-webnovel.com/ajax/chapter-archive?novelId={short_title}"
 
-    # Parse chapter soup, write to file
-    chaptertext = soup.find("div", attrs={"id": "chr-content"}).get_text("\n\n",True)
-       
-    with open(f"{folder}/{titlename} - {chapt.name}.txt", "w") as file:
-        file.write(f"{titlename}\n{chapt.name}\n{chaptertext}")
-    
-    print(f"{chapt.name} - Complete")
+    # Prepare folder
+    folder = path.join(getenv('HOME'), 'WebNovelScraper', titlename)
+    if not path.exists(folder):
+        makedirs(folder)
 
+    # Pull all chapters page
+    response = requests.get(all_chapters, headers=headers)
+    # Raise exception if unsuccessful
+    response.raise_for_status()
 
-# TODO: multiprocessing https://docs.python.org/3/library/multiprocessing.html
+    # Create new soup for chapter list
+    soup = BeautifulSoup(response.text, "html5lib")
 
-if __name__ == '__main__':
+    # Fill list with chapter URLs
+    q = []
+    counter = 1
+    for a in soup.find_all("a", href=True):
+        # chapt = chapter(a.span.contents[0], a['href'])
+        q.append((str(counter), a['href']))
+        counter += 1
+        
     # start worker processes
-    with Pool(processes=cpu_count()) as pool:
-        pool.apply_async(chapter_writer(),(q,))
-        # while not q.empty():
-            # chapter_writer(q.get())       
+    with Pool() as pool:
+        t0 = time()    
+        pool.map(chapter_writer, q)
+        t1 = time()
+        print(f"time taken: {t1-t0}s")
+
+    # TODO: convert output into EPUB format
